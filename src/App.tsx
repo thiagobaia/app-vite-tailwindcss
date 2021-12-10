2b4021c83bb19ca83032e9bd3715f800f04cbff1
Thiago Baia <thiago.baia@valenet.com.br>
2021-12-10 14:05:17 -0300

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 function App() {
   return (
     <div>
-      <div className="bg-orange-500	w-full ...">
-        CONTAINER 
-      </div>
+      <div className="bg-orange-500	w-full ...">CONTAINER</div>
     </div>
-  )
+  );
 }
-export default App
+export default App;
